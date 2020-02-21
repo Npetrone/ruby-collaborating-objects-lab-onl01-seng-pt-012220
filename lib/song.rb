98b0ca4e6936f77250877b8037300de335b27fc3
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-21 03:51:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,11 +19,8 @@ class Song
   
   def self.new_by_filename(file)
     binding.pry
-    name = file.split(" - ")[1]
-    artist = file.split(" - ")[0]
-    song = self.new(song_name)
-    song.artist = artist
-    song
+    name = file.split(" - ")
+    new_song = Song.new(name[1])
   end
   
   def artist_name=(name)
