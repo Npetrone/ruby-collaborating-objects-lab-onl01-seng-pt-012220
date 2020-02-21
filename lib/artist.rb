c24dfa11715ae562a7dd7e136fa6c2c0b993b463
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-21 02:44:26 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name
+  
+  @@all = []
+  @@song_count = 0
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs
+    Song.all.select do |song|
+      song.artist
+    end
+  end
+  
+  def add_song(song)
+   # self.songs << song
+    song.artist = self
+  #  @@song_count += 1
+  
+  end
+  
+
+  
+  
+  
+end
