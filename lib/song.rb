d0678f00ddf91098f5bbdfa28e53f838e02e2d77
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-21 03:48:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+
+
 class Song
   
   attr_accessor :name, :artist
@@ -15,16 +17,16 @@ class Song
   end
   
   
- # def self.new_by_filename(filename)
-   # song_name = filename.split(" - ")[1]
-   # artist = filename.split(" - ")[0]
-   # song = self.new(song_name)
-    #song.artist = artist
-   # song
-  #end
+  def self.new_by_filename(filename)
+    song_name = filename.split(" - ")[1]
+    artist = filename.split(" - ")[0]
+    song = self.new(song_name)
+    song.artist = artist
+    song
+  end
   
- # def artist_name=(name)
-  #  self.artist = Artist.find_or_create_by_name(name)
-   # self.artist.add_song(self)
-  #end
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self)
+  end
 end
